1f07cd8811cb9a3194648da07aec542adc9b105f
ellie timoney <flrl@users.noreply.github.com>
2019-07-25 14:24:42 +1000

    das

--- twitterfix.user.js
+++ twitterfix.user.js
@@ -1,13 +1,23 @@
 // ==UserScript==
 // @name        twitterfix
 // @namespace   https://github.com/flrl/userscripts/
-// @version     4
+// @version     5
 // @grant       none
 // @include     https://twitter.com
 // @include     https://twitter.com/*
 // ==/UserScript==
 
-(function(){
-    'use strict';
-    console.log('hi');
-})();
+'use strict';
+console.log('hi');
+
+var remove_selectors = [
+    'div[aria-label="Timeline: Trending now"',
+    'div[aria-label="Who to follow"]',
+];
+
+remove_selectors.forEach(function(s) {
+    var e = document.querySelector(s);
+    if (e) {
+        e.style.setProperty('border', '1px solid magenta', 'important');
+    }
+});
