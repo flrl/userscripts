f21fcb6f52d81c2646eaea61088719a5f40ed34c
ellie timoney <flrl@users.noreply.github.com>
2019-07-25 13:23:41 +1000

    stub twitterfix

---
+++ twitterfix.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        twitterfix
+// @namespace   https://github.com/flrl/userscripts/
+// @version     0
+// @grant       none
+// @include     https://twitter.com
+// ==/UserScript==
+
+(function(){
+    'use strict';
+    alert('hi');
+})();
