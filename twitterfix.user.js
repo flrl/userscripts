c114e7f5c18eb2d74a0c5fff01c472a33a0bdba4
ellie timoney <flrl@users.noreply.github.com>
2019-07-27 11:47:28 +1000

    ladhfjs

--- twitterfix.user.js
+++ twitterfix.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        twitterfix
 // @namespace   https://github.com/flrl/userscripts/
-// @version     9
+// @version     10
 // @grant       none
 // @include     https://twitter.com
 // @include     https://twitter.com/*
@@ -10,13 +10,17 @@
 'use strict';
 
 var remove_selectors = [
-    'div[aria-label="Timeline: Trending now"',
+    'div[aria-label="Timeline: Trending now"]',
     'aside[aria-label="Who to follow"]',
 ];
 
 var observer = new window.MutationObserver(function() {
-    var sidebar = document.querySelector('div[data-testid="sidebarColumn"]');
-    sidebar.style.setProperty('width', '120px', 'important');
+    // .r-1ovo9ad is where the rhs width: 360px comes from
+    var rhs_widthed = document.querySelectorAll('.r-1ovo9ad');
+    rhs_widthed.forEach(function(e) {
+        console.log(e.style.getProperty('width'));
+        e.style.setProperty('width', '180px', 'important');
+    });
 
     remove_selectors.forEach(function(s) {
         var e = document.querySelector(s);
