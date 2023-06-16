05d19dcebacc9d239fcac8701058be0282cf831e
ellie timoney <flrl@users.noreply.github.com>
2023-06-16 22:25:51 +1000

    twitterfix: remove "Verified"

--- twitterfix.user.js
+++ twitterfix.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        twitterfix
 // @namespace   https://github.com/flrl/userscripts/
-// @version     22
+// @version     23
 // @grant       none
 // @include     https://twitter.com
 // @include     https://twitter.com/*
@@ -19,6 +19,7 @@ var remove_selectors = [
     'div[aria-label="Timeline: "]',
     'a[aria-label$="View Tweet analytics"]',
     'aside[aria-label="Get Verified"]',
+    'a[aria-label="Verified"]',
 ];
 
 var observer = new window.MutationObserver(function() {
